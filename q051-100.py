158091497b9c2c9307312b8a8db4f746a95862a1
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-09-18 10:32:01 -0400

    Q56-60

--- q051-100.py
+++ q051-100.py
@@ -89,16 +89,115 @@ class Solution:
         return False
 
 # Q56 Merge Intervals
-
+class Solution:
+    def merge(self, intervals):
+        #intervals = intervals.sort(key=lambda x: x[0])
+        intervals = sorted(intervals)
+        merged = []
+        for i in intervals:
+            if not merged or merged[-1][1] < i[0]:
+                merged.append(i)
+            else:
+                merged[-1] = [min(min(merged[-1]),min(i)),max(max(i),max(merged[-1]))]
+        return merged
 
 # Q57 Insert Interval
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
 
+    def __repr__(self):
+        return "[{}, {}]".format(self.start, self.end)
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        result = []
+        i = 0
+        while i < len(intervals) and newInterval.start > intervals[i].end:
+            result += intervals[i],
+            i += 1
+        while i < len(intervals) and newInterval.end >= intervals[i].start:
+            newInterval = Interval(min(newInterval.start, intervals[i].start),max(newInterval.end, intervals[i].end))
+            i += 1
+        result += newInterval,
+        result += intervals[i:]
+        return result
 
 # Q58 Length of Last Word
-
+class Solution:
+    def lengthOfLastWord(self, s):
+        return len(s.strip().split(" ")[-1])
 
 # Q59 Spiral Matrix II
+class Solution:
+    def generateMatrix(self, n):
+        total = n**2
+        start = 1
+        matrix = [[0]*n for _ in range(n)]
 
+        left, right, top, bottom = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
+
+        while left <= right and top <= bottom:
+            for j in range(left, right + 1):
+                matrix[top][j] = start
+                start += 1
+            for i in range(top + 1, bottom):
+                matrix[i][right] = start
+                start += 1
+            for j in reversed(range(left, right + 1)):
+                if top < bottom:
+                    matrix[bottom][j] = start
+                    start += 1
+            for i in reversed(range(top + 1, bottom)):
+                if left < right:
+                    matrix[i][left] = start
+                    start += 1
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+        return matrix
 
 # Q60 Permutation Sequence
+import math
 
+class Solution:
+    def getPermutation(self, n, k):
+        seq, k, fact = "", k - 1, math.factorial(n - 1)
+        perm = [i for i in range(1, n + 1)]
+        for i in reversed(range(n)):
+            curr = perm[k // fact]
+            seq += str(curr)
+            perm.remove(curr)
+            if i > 0:
+                k %= fact
+                fact //= i
+        return seq
+
+# Q61
+
+
+# Q62
+
+
+# Q63
+
+
+# Q64
+
+
+# Q65
+
+
+# Q66
+
+
+# Q67
+
+
+# Q68
+
+
+# Q69
+
+
+# Q70
